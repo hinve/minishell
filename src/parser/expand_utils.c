5d76fdfbddc85ef34a04401234c6fd0ed1a2414a
Héctor Pino <140248223+hinve@users.noreply.github.com>
2025-01-13 20:04:24 +0000

    expand on heredoc done

--- src/parser/expand_utils.c
+++ src/parser/expand_utils.c
@@ -19,6 +19,8 @@ static char *append_str(char *dest, const char *src)
     int dl = ft_strlen(dest);
     int sl = ft_strlen(src);
     char *new_str = (char *)malloc(dl + sl + 1);
+    if (!new_str)
+        return (NULL);
     int i = 0;
     while (dest && dest[i])
     {
@@ -32,13 +34,28 @@ static char *append_str(char *dest, const char *src)
     return (new_str);
 }
 
+char *get_env_value2(const char *var, t_env *env)
+{
+    t_env *current = env;
+
+    while (current)
+    {
+        if (ft_strncmp(current->key, var, ft_strlen(var)) == 0 && current->key[ft_strlen(var)] == '\0')
+        {
+            return (current->value);
+        }
+        current = current->next;
+    }
+    return ("");
+}
+
 char *get_env_var(char *line, int *i, t_env *env)
 {
     int start = *i + 1;
     while (line[*i] && (ft_isalnum(line[*i]) || line[*i] == '_'))
         (*i)++;
     char *var_name = ft_substr(line, start, *i - start);
-    char *value = get_value(env, var_name);
+    char *value = get_env_value2(var_name, env);
     free(var_name);
     return value ? value : "";
 }
@@ -47,16 +64,25 @@ char *expand_heredoc(char *line, t_env *env)
 {
     int i = 0;
     char *temp = ft_strdup("");
+    if (!temp)
+        return (NULL);
     while (line && line[i])
     {
+        printf("Processing character: %c\n", line[i]); // Mensaje de depuración
         if (line[i] == '$')
         {
             char *value = get_env_var(line, &i, env);
+            printf("Found variable: %s\n", value); // Mensaje de depuración
             temp = append_str(temp, value);
+            int j = 0;
+            while(line[j]) {
+                i++;
+                j++;
+            }
         }
         else if (line[i] == '~' && (!line[i + 1] || line[i + 1] == '/'))
         {
-            temp = append_str(temp, get_value(env, "HOME"));
+            temp = append_str(temp, get_env_value2("HOME", env));
             i++;
         }
         else
@@ -66,5 +92,6 @@ char *expand_heredoc(char *line, t_env *env)
             i++;
         }
     }
+    printf("Expanded line: %s\n", temp); // Mensaje de depuración
     return (temp);
 }
